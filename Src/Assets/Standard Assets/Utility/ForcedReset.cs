ada2b22402eb5f6b8ed87f73bb0678d9fb00da87
aguzmanc <spacerockganimedes@gmail.com>
2025-09-11 11:38:18 -0400

    Standard Assets

--- Src/Assets/Standard Assets/Utility/ForcedReset.cs
+++ Src/Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,9 +1,8 @@
 using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
+//[RequireComponent(typeof (GUITexture))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
@@ -12,7 +11,7 @@ public class ForcedReset : MonoBehaviour
         if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
         {
             //... reload the scene
-            SceneManager.LoadScene(SceneManager.GetSceneAt(0).path);
+            Application.LoadLevelAsync(Application.loadedLevelName);
         }
     }
 }
